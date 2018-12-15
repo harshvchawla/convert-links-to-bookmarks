32dac47006da5ee90e6b4363aad0c32f68fe2347
Harsh Chawla <harshvchawla@yahoo.co.in>
2018-12-15 16:21:41 +0530

    script to convert links file into bookmark file ready to be imported

---
+++ convertLinksToBookmarks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+echo "usage: $0 <txt file containing list of urls>"
+inputFile=${1}
+if [ $# -ne 1 ] || [ ! -f ${inputFile} ];then
+    echo 'check usage'
+    exit -1
+fi
+
+outputFile='bookmarks-scripted.html'
+echo '<DL>' > $outputFile
+NumLines=`wc -l ${inputFile} | cut -d' ' -f1`
+currLineNum=0
+
+for eachUrl in `cat ${inputFile}`
+do
+    let currLineNum=${currLineNum}+1
+    echo "processing Line ${currLineNum} of ${NumLines}..."
+    
+    urlTitle=`echo $eachUrl | rev | cut -d'/' -f1 | rev`
+    if [ -z ${urlTitle} ];then
+        urlTitle=`echo $eachUrl | cut -d'.' -f2`
+    fi
+    
+    echo ${urlTitle}
+    urlLine='<DT><A HREF="'
+    urlLine+=${eachUrl}
+    urlLine+='">'
+    urlLine+=${urlTitle}
+    urlLine+='</A>'
+    echo ${urlLine} >> $outputFile
+    echo
+done
+
+echo '</DL>' >> $outputFile
